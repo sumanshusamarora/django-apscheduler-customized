1eb1eb0c74422b952846586f64d47b068e4f0d3a
Sumanshu Arora <45642655+sumanshusamarora@users.noreply.github.com>
2020-06-29 12:33:30 +1000

    Create apps.py

---
+++ apps.py
@@ -0,0 +1,22 @@
+from django.apps import AppConfig
+from django.conf import settings
+from apscheduler.schedulers.background import BackgroundScheduler
+from pytz import timezone
+from loan_iq.config import logging
+logger = logging.getLogger(__name__)
+
+tz = timezone('Australia/Melbourne')
+scheduler = BackgroundScheduler()
+
+class DjangoApschedulerConfig(AppConfig):
+    name = 'django_apscheduler'
+    verbose_name = "Django APScheduler"
+
+    def ready(self):
+        from django_apscheduler.jobstores import DjangoJobStore, register_events
+        scheduler.add_jobstore(DjangoJobStore(), 'default')
+        logger.info("Django job store added to scheduler")
+        register_events(scheduler)
+        if settings.SCHEDULER_AUTOSTART:
+            scheduler.start()
+            logger.info("Scheduler started!")
